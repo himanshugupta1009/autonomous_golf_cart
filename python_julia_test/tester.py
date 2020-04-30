f69eae05b20c5f86ca74fbe3f21cb4f80313f70d
dkiit07 <dk.iit07@gmail.com>
2020-04-30 04:25:30 -0600

    refactoring

--- python_julia_test/tester.py
+++ python_julia_test/tester.py
@@ -1,19 +1,17 @@
 from julia import Julia
 jul = Julia(compiled_modules=False)
 
-ds = """
-function lala(a,b)
-    return a+b
-end
-
-"""
-
-
-
-
+# ds = """
+# function lala(a,b)
+#     return a+b
+# end
+#
+# """
 
 x = jul.include("test.jl")
 # y = jul.eval(ds)
 
 # y = jul.eval("test.jl")
+
+
 print(x(1,2))
